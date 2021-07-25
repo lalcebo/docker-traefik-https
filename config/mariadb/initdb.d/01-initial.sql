fbef7eb7addbdb95f053347bd0970ed0e05d93ab
Jorge P. Hernandez Lalcebo <lalcebo2003@gmail.com>
2021-07-25 15:48:11 -0400

    Pull apart MySQL & MariaDB  initial sql files.

--- config/mariadb/initdb.d/01-initial.sql
+++ config/mariadb/initdb.d/01-initial.sql
@@ -6,5 +6,11 @@ FROM `user`
 WHERE `user` = 'root'
   AND `host` <> '%';
 
+-- Update root user with local access with password.
+UPDATE `user`
+SET `host` = '%'
+WHERE `user` = 'root'
+  AND `plugin` = 'mysql_native_password';
+
 -- Reload privileges
 FLUSH PRIVILEGES;
