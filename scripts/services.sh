1ca15922beb45f052e5ec1d6600d4f4c01e3b266
Jorge P. Hernandez Lalcebo <lalcebo2003@gmail.com>
2022-02-05 23:21:43 -0500

    feat: add hooks system.

--- scripts/services.sh
+++ scripts/services.sh
@@ -2,18 +2,32 @@
 
 # shellcheck disable=SC2039
 # shellcheck disable=SC2206
+# shellcheck disable=SC1090
 
 # Applications
 APPS=($ENABLE_APPS)
 for i in "${APPS[@]}"; do
+    # load pre hooks
+    if [ -f "./hooks/apps/pre-$i" ]; then
+        . "./hooks/apps/pre-$i"
+    fi
     COMPOSE_FILE=${COMPOSE_FILE}${PATH_SEPARATOR}./apps/${i}.yml
+    # load post hooks
+    if [ -f "./hooks/apps/post-$i" ]; then
+        . "./hooks/apps/post-$i"
+    fi
 done
 
 # Servers
 SERVERS=($ENABLE_SERVERS)
 for i in "${SERVERS[@]}"; do
-    if [ "$i" = "dynamodb" ] && [ ! -d .docker/dynamodb ]; then
-        mkdir -p .docker/dynamodb && chmod -R 777 .docker/dynamodb
+    # load pre hooks
+    if [ -f "./hooks/servers/pre-$i" ]; then
+        . "./hooks/servers/pre-$i"
     fi
     COMPOSE_FILE=${COMPOSE_FILE}${PATH_SEPARATOR}./servers/${i}.yml
+    # load post hooks
+    if [ -f "./hooks/servers/post-$i" ]; then
+        . "./hooks/servers/post-$i"
+    fi
 done
